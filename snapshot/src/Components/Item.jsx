ea31aede88faa389f3605dd47111324826c1b43f
SirCrewete <gimenocrew1996@gmail.com>
2022-06-02 20:08:18 +0200

    Start

---
+++ snapshot/src/Components/Item.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Styles/Item.css'
+
+function Item () {
+    return (
+        <div></div>
+    )
+}
+
+export default Item;
---
+++ snapshot/src/Components/Pantalla.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Styles/Pantalla.css'
+
+
+function Pantalla () {
+    return (
+        <div className='container-pantalla'></div>
+    )
+}
+
+export default Pantalla;
